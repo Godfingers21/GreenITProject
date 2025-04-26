21fa80368d8e24eea6e499e2fe68f57611d2f99b
felix <proemail78@gmail.com>
2025-04-26 22:54:28 +0200

    test vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),vercel()],
   server: {
     proxy: {
       '/api': {
